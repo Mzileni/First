704177c33c811dc00674e8a9999f5653d1770cba
Mzileni <216279631@mycput.ac.za>
2020-02-18 12:40:10 -1000

    I just made some changes

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,7 +1,10 @@
 public class Exercise1 {
 
     public static void main(String[] args) {
+
         System.out.println("Hello I have arrived");
+
+        System.out.println("Hey, welcome to 3rd year");
     }
 
 }
